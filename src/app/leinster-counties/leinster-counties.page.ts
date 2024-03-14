7a9471a95a78100509af467f4cc5d38418583ab1
Karolis B2 <G00417529@atu.ie>
2024-03-14 14:01:13 +0000

    first commit

---
+++ src/app/leinster-counties/leinster-counties.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterLinkWithHref } from '@angular/router';
+
+@Component({
+  selector: 'app-leinster-counties',
+  templateUrl: './leinster-counties.page.html',
+  styleUrls: ['./leinster-counties.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, RouterLinkWithHref]
+})
+export class LeinsterCountiesPage implements OnInit {
+  counties: string[] = ["Carlow", "Dublin", "Kildare",
+  "Kilkenny", "Laois", "Longford", "Louth", "Meath",
+  "Offaly", "Westmeath", "Wexford", "Wicklow"];
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
